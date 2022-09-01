eccdb7717375c0718825d76e7ab3ad1512878a40
Ian Lucas de Azevedo Carneiro <103367370+IanLucas4515@users.noreply.github.com>
2022-08-31 22:10:43 -0300

    Atualizando E-mail

--- Gestao_Novo/core/settings.py
+++ Gestao_Novo/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 #from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,3 +135,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR/'templates/static'
 ]
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
