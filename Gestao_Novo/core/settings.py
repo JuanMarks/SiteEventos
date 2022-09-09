887b7a6c2062205b712ad48da4c5eefd6cfa735f
JuanMarks <82836871+JuanMarks@users.noreply.github.com>
2022-09-09 18:53:17 -0300

    Fazendo deploy teste no heroku

--- Gestao_Novo/core/settings.py
+++ Gestao_Novo/core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import django_heroku
 #from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -144,3 +145,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+django_heroku.settings(locals())
--- Gestao_Novo/ninna/Scripts/activate.bat
+++ Gestao_Novo/ninna/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\joels\Documents\DJANGO\PROJETO_NINNA\SiteEventos\Gestao_Novo\ninna
+set VIRTUAL_ENV=C:\Users\juanp\Desktop\besteira\SiteEventos\Gestao_Novo\ninna
 
 if not defined PROMPT set PROMPT=$P$G
 
