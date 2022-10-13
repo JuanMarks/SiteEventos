3c137dcf62b96ee8c12cc771e7df7e5dba41f033
Ian Lucas de Azevedo Carneiro <103367370+IanLucas4515@users.noreply.github.com>
2022-10-13 19:02:48 -0300

    Algumas modificações

--- Gestao_Novo/enviar/models.py
+++ Gestao_Novo/enviar/models.py
@@ -6,4 +6,9 @@ class Usuario(models.Model):
     nome = models.CharField(max_length=100)
     e_mail = models.EmailField()
     grupo = models.CharField(max_length=100)
-    
+
+class RecuperarSenha(models.Model):
+    email_r = models.EmailField()
+    codigo = models.IntegerField(unique=True)
+    data = models.DateTimeField(auto_now_add=True)
+    data_vencimento = models.DateTimeField()
